6f05e83d5a2b1714b728922092de18ed0e907515
Amarnathamraj <amarnath987654@gmail.com>
2024-02-03 14:44:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function handleFormSubmit(event){
+    event.preventDefault();
+    const name=event.target.username.value;
+    const mail=event.target.email.value;
+    const telephone=event.target.phone.value;
+    const myobj={
+        username:name,
+        email:mail,
+        phone:telephone
+    };
+    const obj=JSON.stringify(myobj);
+    localStorage.setItem("User Details",obj);
+}
+module.exports=handleFormSubmit;
